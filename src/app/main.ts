a81f863c3764e3d9e45cd8cfd5a177c3e4baf20f
Karen Olson <karmarie@gmail.com>
2025-05-05 18:01:16 -0500

    skipUntil operator example
    This is a transformation operator. It takes an Observable stream and creates
    a new one that emits the same events, but doesn't start emitting until a second
    Observable stream starts emitting events.

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,34 +1,32 @@
-import {AsyncSubject} from 'rxjs';
+import {Observable, Subject, skipUntil} from 'rxjs';
 import {addItem} from "../helpers";
 
-// REPLAY SUBJECT WITH WINDOW TIME
-//   -> Can read and emit values, so it is both an Observable and Observer.
-//   -> Observers only receive the last emitted event plus any completion event.
-const subject = new AsyncSubject();
+const producer1 = (observer: any) => {
+    var i = 1
+    setInterval(() => {
+        observer.next(i++)
+    }, 1000)
+}
 
-// OBSERVER 1
+// Both observables are created at the same time.
+//      Observable 1 starts emitting values right away, every 1 second
+//      Observable 2 doesn't emit any values for 3s.
+const observable1 = new Observable(producer1);
+const observable2 = new Subject();
+
+setTimeout(() => {
+    observable2.next('Hey!')
+}, 3000)
+
 const observer1 = {
     next: (data: any) => addItem('Observer 1: ' + data),
     error: (err: any) => addItem('Observer 1: ' + err),
     complete: () => addItem('Observer 1: Complete')
 }
-const subscription1 = subject.subscribe(observer1);
 
-// PRODUCING EVENTS
-//      Observers 1 & 2 only receive the last event.
-var i = 1;
-var interval = setInterval(() => {
-    subject.next(`Thing ${i++}`);
-}, 100);
-
-// OBSERVER 2
-const observer2 = {
-    next: (data: any) => addItem('Observer 2: ' + data),
-    error: (err: any) => addItem('Observer 2: ' + err),
-    complete: () => addItem('Observer 2: Complete')
-}
-
-setTimeout(() => {
-    const subscription2 = subject.subscribe(observer2);
-    subject.complete();
-}, 500)
+// Observable 3 is a transformation of Observable 1.
+//      It emits the same values as Observable 1, but doesn't start emitting until Observable 2 emits something.
+const observable3 =
+    observable1
+        .pipe(skipUntil(observable2))
+        .subscribe(observer1);

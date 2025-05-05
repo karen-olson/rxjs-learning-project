62732a11b17b1f8bee5da0ea34b0d876ce91c6cf
Karen Olson <karmarie@gmail.com>
2025-05-05 17:00:37 -0500

    Save basicSubject example

---
+++ src/app/basicSubject.ts
@@ -0,0 +1,34 @@
+import {Subject} from 'rxjs';
+import {addItem} from "../helpers";
+
+// SUBJECT -> Can read and emit values, so it is both an Observable and Observer.
+const subject = new Subject();
+
+// OBSERVER 1
+const observer1 = {
+    next: (data: any) => addItem('Observer 1: ' + data),
+    error: (err: any) => addItem('Observer 1: ' + err),
+    complete: () => addItem('Observer 1: Complete')
+}
+const subscription1 = subject.subscribe(observer1);
+
+// PRODUCING EVENTS - ONLY GOES TO OBSERVER 1
+subject.next('The first thing has been sent');
+
+// OBSERVER 2
+const observer2 = {
+    next: (data: any) => addItem('Observer 2: ' + data),
+    error: (err: any) => addItem('Observer 2: ' + err),
+    complete: () => addItem('Observer 2: Complete')
+}
+const subscription2 = subject.subscribe(observer2);
+
+// PRODUCING EVENTS - GO TO OBSERVERS 1 & 2
+subject.next('The second thing has been sent');
+subject.next('The third thing has been sent');
+
+// UNSUBSCRIBING OBSERVER 2
+subscription2.unsubscribe();
+
+// SENDING ANOTHER EVENT
+subject.next('The fourth thing has been sent');

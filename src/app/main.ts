45f7698627574a917532836f25393e7ffa49479b
Karen Olson <karmarie@gmail.com>
2025-05-05 15:06:31 -0500

    Refactoring for readability

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,10 +1,11 @@
 import {Observable} from 'rxjs';
 import {addItem} from "../helpers";
-import {Greeting, NotSent} from "../Messages";
+import {Greeting} from "../Messages";
 import {GreetingObserver} from "../GreetingObserver"
 
-// Observable
-const subscribeToGreeting = (observer: GreetingObserver) => {
+// PRODUCER & OBSERVABLE
+//   Producer
+const produceGreetings = (observer: GreetingObserver) => {
     try {
         observer.next(Greeting.hello)
         observer.next(Greeting.howAreYou)
@@ -15,29 +16,33 @@ const subscribeToGreeting = (observer: GreetingObserver) => {
         observer.error(err)
     }
 }
-const greetingObservable = new Observable(subscribeToGreeting);
+//    Observable
+const greetingObservable = new Observable(produceGreetings);
 
-// Observers & Subscriptions
-const greetingObserver1: GreetingObserver = {
-    next: (greeting: Greeting) => addItem(greeting),
-    error: (err: any) => addItem(err),
-    complete: () => addItem("Complete")
+// OBSERVERS & SUBSCRIPTIONS
+const secondsPastCurrentMinute = () => new Date().getSeconds();
+
+const createGreetingObserver = (subscriberName: string): GreetingObserver => {
+    return {
+        next: (greeting: Greeting) => addItem(`${subscriberName}: (${secondsPastCurrentMinute()})` + greeting),
+        error: (err: any) => addItem(`${subscriberName}: (${secondsPastCurrentMinute()})` + err),
+        complete: () => addItem(`${subscriberName}: (${secondsPastCurrentMinute()}) Complete`)
+    }
 }
-const greetingSubscription1 = greetingObservable.subscribe(greetingObserver1);
 
+//    Observers
+const greetingObserver1: GreetingObserver = createGreetingObserver('Subscriber 1');
+const greetingObserver2: GreetingObserver = createGreetingObserver('Subscriber 2');
+
+//    Subscriptions
+const greetingSubscription1 = greetingObservable.subscribe(greetingObserver1);
+const greetingSubscription2 = greetingObservable.subscribe(greetingObserver2);
+
+//    Unsubscribing After Timeout
 setTimeout(() => {
     greetingSubscription1.unsubscribe();
-}, 3001)
-
-
-const greetingObserver2: GreetingObserver = {
-    next: (greeting: Greeting) => addItem(greeting),
-    error: (err: any) => addItem(err),
-    complete: () => addItem("Complete")
-}
-const greetingSubscription2 = greetingObservable.subscribe(greetingObserver2);
+}, 3001);
 
 setTimeout(() => {
     greetingSubscription2.unsubscribe();
-}, 5001)
-
+}, 5001);

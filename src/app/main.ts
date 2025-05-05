ca4315c30a3917cd7eb51001e0937345c6f6bf1f
Karen Olson <karmarie@gmail.com>
2025-05-05 17:22:21 -0500

    Replay Subscriber example 2 - emits pre-subscription events within time window

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,10 +1,10 @@
 import {ReplaySubject} from 'rxjs';
 import {addItem} from "../helpers";
 
-// REPLAY SUBJECT
+// REPLAY SUBJECT WITH WINDOW TIME
 //   -> Can read and emit values, so it is both an Observable and Observer.
-//   -> Subscribers get the designated number of values emitted before their subscription started.
-const subject = new ReplaySubject(2);
+//   -> Subscribers get pre-subscription events emitted within the specified time window.
+const subject = new ReplaySubject(1000, 500);
 
 // OBSERVER 1
 const observer1 = {
@@ -15,12 +15,12 @@ const observer1 = {
 const subscription1 = subject.subscribe(observer1);
 
 // PRODUCING EVENTS
-//      Observer 1 receives things 1-4 because it was created before event emissions
-//      Observer 2 receives things 3 & 4 because of buffer size 2
-subject.next('Thing 1');
-subject.next('Thing 2');
-subject.next('Thing 3');
-subject.next('Thing 4');
+//      Observer 1 receives all events because it was created before event emissions
+//      Observer 2 receives up to 2 events prior to its creation (at 500ms) if they happened within 2s before subscription.
+var i = 1;
+var interval = setInterval(() => {
+    subject.next(`Thing ${i++}`);
+}, 100);
 
 // OBSERVER 2
 const observer2 = {
@@ -28,9 +28,7 @@ const observer2 = {
     error: (err: any) => addItem('Observer 2: ' + err),
     complete: () => addItem('Observer 2: Complete')
 }
-const subscription2 = subject.subscribe(observer2);
 
-// PRODUCING EVENTS
-//      Both observers receive thing 5 & 6
-subject.next('Thing 5');
-subject.next('Thing 6');
+setTimeout(() => {
+    const subscription2 = subject.subscribe(observer2);
+}, 500)

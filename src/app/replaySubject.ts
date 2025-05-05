b9137bd459825a065bdc5baf48b054fca3ffba0b
Karen Olson <karmarie@gmail.com>
2025-05-05 17:11:55 -0500

    Save Replay Subject example

---
+++ src/app/replaySubject.ts
@@ -0,0 +1,36 @@
+import {ReplaySubject} from 'rxjs';
+import {addItem} from "../helpers";
+
+// REPLAY SUBJECT
+//   -> Can read and emit values, so it is both an Observable and Observer.
+//   -> Subscribers get the designated number of values emitted before their subscription started.
+const subject = new ReplaySubject(2);
+
+// OBSERVER 1
+const observer1 = {
+    next: (data: any) => addItem('Observer 1: ' + data),
+    error: (err: any) => addItem('Observer 1: ' + err),
+    complete: () => addItem('Observer 1: Complete')
+}
+const subscription1 = subject.subscribe(observer1);
+
+// PRODUCING EVENTS
+//      Observer 1 receives things 1-4 because it was created before event emissions
+//      Observer 2 receives things 3 & 4 because of buffer size 2
+subject.next('Thing 1');
+subject.next('Thing 2');
+subject.next('Thing 3');
+subject.next('Thing 4');
+
+// OBSERVER 2
+const observer2 = {
+    next: (data: any) => addItem('Observer 2: ' + data),
+    error: (err: any) => addItem('Observer 2: ' + err),
+    complete: () => addItem('Observer 2: Complete')
+}
+const subscription2 = subject.subscribe(observer2);
+
+// PRODUCING EVENTS
+//      Both observers receive thing 5 & 6
+subject.next('Thing 5');
+subject.next('Thing 6');

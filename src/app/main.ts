c9ef00e7eaeb3fba394492177dbd1456dd91f771
Karen Olson <karmarie@gmail.com>
2025-05-05 15:40:15 -0500

    Cold observable demo with second subscription after timeout

--- src/app/main.ts
+++ src/app/main.ts
@@ -3,7 +3,7 @@ import {addItem} from "../helpers";
 import {Greeting} from "../Messages";
 import {GreetingObserver} from "../GreetingObserver"
 
-// PRODUCER & OBSERVABLE
+// PRODUCER & COLD OBSERVABLE
 //   Producer
 const produceGreetings = (observer: GreetingObserver) => {
     try {
@@ -16,33 +16,28 @@ const produceGreetings = (observer: GreetingObserver) => {
         observer.error(err)
     }
 }
-//    Observable
+//    Cold Observable
 const greetingObservable = new Observable(produceGreetings);
 
 // OBSERVERS & SUBSCRIPTIONS
+//   Helpers
 const secondsPastCurrentMinute = () => new Date().getSeconds();
-
 const createGreetingObserver = (subscriberName: string): GreetingObserver => {
     return {
-        next: (greeting: Greeting) => addItem(`${subscriberName}: (${secondsPastCurrentMinute()})` + greeting),
-        error: (err: any) => addItem(`${subscriberName}: (${secondsPastCurrentMinute()})` + err),
-        complete: () => addItem(`${subscriberName}: (${secondsPastCurrentMinute()}) Complete`)
+        next: (greeting: Greeting) => addItem(`${subscriberName}: (${secondsPastCurrentMinute()}s) ` + greeting),
+        error: (err: any) => addItem(`${subscriberName}: (${secondsPastCurrentMinute()}s) ` + err),
+        complete: () => addItem(`${subscriberName}: (${secondsPastCurrentMinute()}s) Complete`)
     }
 }
 
 //    Observers
-const greetingObserver1: GreetingObserver = createGreetingObserver('Subscriber 1');
-const greetingObserver2: GreetingObserver = createGreetingObserver('Subscriber 2');
+const greetingObserver1: GreetingObserver = createGreetingObserver('SUBSCRIBER 1');
+const greetingObserver2: GreetingObserver = createGreetingObserver('SUBSCRIBER 2');
 
 //    Subscriptions
 const greetingSubscription1 = greetingObservable.subscribe(greetingObserver1);
-const greetingSubscription2 = greetingObservable.subscribe(greetingObserver2);
 
-//    Unsubscribing After Timeout
+//    Create A Second Subscription after 1 second
 setTimeout(() => {
-    greetingSubscription1.unsubscribe();
-}, 3001);
-
-setTimeout(() => {
-    greetingSubscription2.unsubscribe();
-}, 5001);
+    const greetingSubscription2 = greetingObservable.subscribe(greetingObserver2);
+}, 1000);
